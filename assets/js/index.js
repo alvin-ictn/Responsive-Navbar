689b00691327054a0a145f0c406e0a825973b82b
Alvin Mantovani <alvin.ictn@gmail.com>
2020-09-14 23:46:56 +0700

    remove unused css and JavaScript

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,15 @@
-// let imgDOM = document.querySelector('#header_img');
-// let headerDOM = document.querySelector('header');
-// let coverDOM = document.querySelector('.navigation--heroimage__cover');
 let burger = document.querySelector('.navigation--bar--menu--burger');
 let collapseMenu = document.querySelector('.navigation--bar--menu');
 
-burger.addEventListener('click', function(){
-  collapseMenu.style.display = collapseMenu.style.display === "flex" ? "none" : "flex";
-  ["fill","color"].map(item => {
-    this.parentNode.style[item] = this.parentNode.style[item] === "black" ? "white" : "black";
-  })
-  this.parentNode.style.position = this.parentNode.style.position === "fixed" ? "absolute" : "fixed"
+burger.addEventListener('click', function () {
+	collapseMenu.style.display = collapseMenu.style.display === "flex" ? "none" : "flex";
+	["fill", "color"].map(item => {
+		this.parentNode.style[item] = this.parentNode.style[item] === "black" ? "white" : "black";
+	})
+	this.parentNode.style.position = this.parentNode.style.position === "fixed" ? "absolute" : "fixed"
 })
 
 
-window.onresize = function(){
-  collapseMenu.style.display = window.innerWidth >= 768 ? "flex" : "none"
+window.onresize = function () {
+	collapseMenu.style.display = window.innerWidth >= 768 ? "flex" : "none"
 }
-
-// window.onload = function(){
-//   console.log(window.of)
-//   coverDOM.style.height = `${window.innerHeight < imgDOM.height ? window.innerHeight : imgDOM.height}px`;
-//   headerDOM.style.height = `${window.innerHeight < imgDOM.height ? window.innerHeight : imgDOM.height}px`;
-// }
-
-// window.onresize = function(){
-//   coverDOM.style.height = `${window.innerHeight < imgDOM.height ? window.innerHeight : imgDOM.height}px`;
-//   headerDOM.style.height = `${window.innerHeight < imgDOM.height ? window.innerHeight : imgDOM.height}px`;
-//   console.log(headerDOM.style.height);
-// }

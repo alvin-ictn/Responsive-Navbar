df557c26c8e53b857fed19689695de3c730fe472
Alvin Mantovani <alvin.ictn@gmail.com>
2020-09-14 22:26:45 +0700

    working on hamburger collapse menu and finishing the HTML

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // let imgDOM = document.querySelector('#header_img');
 // let headerDOM = document.querySelector('header');
 // let coverDOM = document.querySelector('.navigation--heroimage__cover');
+let burger = document.querySelector('.navigation--bar--menu--burger');
+console.log(burger)
 
+burger.addEventListener('click', function(){
+  console.log(this)
+})
 // window.onload = function(){
 //   console.log(window.of)
 //   coverDOM.style.height = `${window.innerHeight < imgDOM.height ? window.innerHeight : imgDOM.height}px`;

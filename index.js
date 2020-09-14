c74f1af3a6537c2a55dd5c40335c5aac28c079f1
Alvin Mantovani <alvin.ictn@gmail.com>
2020-09-14 23:32:36 +0700

    finish the responsive and hamburger menu collapse

--- index.js
+++ index.js
@@ -2,11 +2,21 @@
 // let headerDOM = document.querySelector('header');
 // let coverDOM = document.querySelector('.navigation--heroimage__cover');
 let burger = document.querySelector('.navigation--bar--menu--burger');
-console.log(burger)
+let collapseMenu = document.querySelector('.navigation--bar--menu');
 
 burger.addEventListener('click', function(){
-  console.log(this)
+  collapseMenu.style.display = collapseMenu.style.display === "flex" ? "none" : "flex";
+  ["fill","color"].map(item => {
+    this.parentNode.style[item] = this.parentNode.style[item] === "black" ? "white" : "black";
+  })
+  this.parentNode.style.position = this.parentNode.style.position === "fixed" ? "absolute" : "fixed"
 })
+
+
+window.onresize = function(){
+  collapseMenu.style.display = window.innerWidth >= 768 ? "flex" : "none"
+}
+
 // window.onload = function(){
 //   console.log(window.of)
 //   coverDOM.style.height = `${window.innerHeight < imgDOM.height ? window.innerHeight : imgDOM.height}px`;
